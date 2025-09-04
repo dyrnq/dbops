2f370ace965345f5dd20a75978dcd022494da2ab
dyrnq <dyrnq@qq.com>
2025-09-04 11:55:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -Eeo pipefail
 
 
-if ! /tmp/xq --version ; then
+if ! /tmp/xq --version >/dev/null 2>&1; then
   curl -fSL# --retry 10 https://github.com/sibprogrammer/xq/releases/download/v1.2.5/xq_1.2.5_linux_amd64.tar.gz | tar -xvz -C /tmp
 fi
 
-ver=$(cat pom.xml | /tmp/xq -x /project/version)
+ver=$(/tmp/xq -x /project/version < pom.xml)
 echo "ver=${ver}"
 mvn clean package -Dmaven.test.skip=true --file pom.xml
